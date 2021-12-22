ed32ccaaa72c20ca5a71fd7a33011a11651213e6
angeruzzi <alessandro@angeruzzi.com.br>
2021-12-21 21:11:09 -0300

    Pipeline das ações disponíveis

---
+++ source/pipeline.py
@@ -0,0 +1,59 @@
+import pandas as pd 
+from sklearn.linear_model import LogisticRegression
+
+import loader as ld
+import cleaner as cl
+import vectorizer as vc
+import modeler as md
+
+#CONFIGURAÇÕES
+PATH_CORPUS  = '../corpus'
+PATH_PREDICT = '../topredict' 
+TYPE_CONT	 = 'count' #Tfidf
+
+def train():
+
+	modelo_base = LogisticRegression()
+
+	dfTrain   = ld.loadTrain(PATH_CORPUS)
+	dfTrain   = cl.cleanData(dfTrain)
+	bow_train = vc.vectTrain(dfTrain, TYPE_CONT)
+	md.trainModel(modelo_base, bow_train, dfTrain['target'], True)
+
+	return "Modelo Treinado"
+
+
+def trainAndTest():
+
+	modelo_base = LogisticRegression()
+
+	#Treino
+	dfTrain   = ld.loadTrain(PATH_CORPUS)
+	dfTrain   = cl.cleanData(dfTrain)
+	bow_train = vc.vectTrain(dfTrain, TYPE_CONT)
+	modelo_treinado = md.trainModel(modelo_base, bow_train, dfTrain['target'], True)
+
+	#Teste
+	dfTest   = ld.loadTest(PATH_CORPUS)
+	dfTest   = cl.cleanData(dfTest)
+	bow_test = vc.vectTest(dfTest, TYPE_CONT)
+	result   = md.testModel(modelo_treinado, bow_test, dfTest['target'])
+
+	return result
+	
+
+def predictSaveModel():
+
+	dfPred   = ld.loadPredict(PATH_PREDICT)
+	dfPred   = cl.cleanData(dfPred, 'text', 'text_pos')
+	bow_pred = vc.vectPredict(dfPred, TYPE_CONT, 'text_pos')
+	preditos = md.predictSaveModel(bow_pred)
+
+	if preditos is not None:
+
+		dfRet    = dfPred[['name','text']]
+		dfRet['prediction'] = preditos
+		return dfRet
+
+	return None
+	
